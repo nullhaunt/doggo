163a18f7e640e350e456627c9ab7664099aac11c
Nullhaunt <292233993+nullhaunt@users.noreply.github.com>
2026-09-03 18:49:00 -0700

    CMake warnings

---
+++ cmake/DoggoWarnings.cmake
@@ -0,0 +1,33 @@
+add_library(doggo_warnings INTERFACE)
+
+add_library(doggo::warnings ALIAS doggo_warnings)
+
+if (MSVC)
+    target_compile_options(
+            doggo_warnings
+            INTERFACE
+            /W4
+            /permissive-
+            /Zc:__cplusplus
+    )
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    target_compile_options(
+            doggo_warnings
+            INTERFACE
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wshadow
+            -Wundef
+            -Wformat=2
+            -Wcast-align
+            -Wnon-virtual-dtor
+            -Woverloaded-virtual
+    )
+else ()
+    message(
+            FATAL_ERROR
+            "DOGGO does not currently define a warning policy for compiler: "
+            "${CMAKE_CXX_COMPILER_ID}"
+    )
+endif ()

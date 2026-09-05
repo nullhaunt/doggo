3cb1da819aee89056b1953b4b8fce75e88916c97
Nullhaunt <292233993+nullhaunt@users.noreply.github.com>
2026-09-04 20:20:36 -0700

    Move mesh sample ownership out of platform

--- samples/02-mesh/WindowsMain.cpp
+++ samples/02-mesh/WindowsMain.cpp
@@ -1,11 +1,11 @@
-#include "WindowsApplication.hpp"
 #include "RenderBackend.hpp"
+#include "WindowsApplication.hpp"
 
 int main()
 {
     doggo::platform::windows::WindowsApplication host;
     doggo::input::NullBackend                    input;
-    doggo::render::NullBackend                   render;
+    doggo::render::NullBackend                   renderer;
 
-    return doggo::run( host, input, render );
+    return doggo::run( host, input, renderer );
 }
